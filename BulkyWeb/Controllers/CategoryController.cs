60631ba9747646b014666ed255f4e5180021713c
RobertMcDougall <rtmcdougall@yahoo.co.nz>
2025-03-14 10:29:23 +1300

    CRUD functions has been added and is operational.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -18,7 +18,8 @@ namespace BulkyWeb.Controllers
             var objCatergoryList = _db.Categories.ToList();
             return View(objCatergoryList);
         }
-        
+
+        //Create
         public IActionResult Create()
         {
             return View();
@@ -26,7 +27,77 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category categoryobj)
         {
-            _db.Categories.Add(categoryobj);
+            if (ModelState.IsValid) 
+            {
+                _db.Categories.Add(categoryobj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Edit
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0) 
+            {
+                return NotFound();
+            }
+            
+            //Find the id
+            Category? categoryFromDB = _db.Categories.Find(id);
+
+            //There's Two more approaches to finding the id
+                //Category? categoryFromDB = _db.Categories.FirstOrDefault(u => u.Id == id);
+                //Category? categoryFromDB = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category categoryobj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(categoryobj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Find the id
+            Category? categoryFromDB = _db.Categories.Find(id);
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? categoryobj = _db.Categories.Find(id);
+            if (categoryobj == null)
+            {
+                return NotFound();
+
+            }
+            _db.Categories.Remove(categoryobj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
